75b4feff1253851263e7462761bbe372d9c35a4e
Jonas Pleyer <jonas.dev@pleyer.org>
2025-08-31 00:53:35 +0200

    add source code for abm simulation

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+mod bacterial_properties;
+mod simulation;
+
+use bacterial_properties::*;
+use simulation::*;
+
+use pyo3::prelude::*;
+
+#[pymodule]
+fn cr_pool(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(run_simulation, m)?)?;
+    m.add_function(wrap_pyfunction!(generate_cells, m)?)?;
+
+    m.add_class::<Bacteria>()?;
+    m.add_class::<BacteriaTemplate>()?;
+    m.add_class::<Species>()?;
+    m.add_class::<cellular_raza::prelude::NewtonDamped2D>()?;
+    m.add_class::<BacteriaCycle>()?;
+    m.add_class::<BacteriaReactions>()?;
+
+    m.add_class::<MetaParams>()?;
+    m.add_class::<Domain>()?;
+
+    Ok(())
+}
